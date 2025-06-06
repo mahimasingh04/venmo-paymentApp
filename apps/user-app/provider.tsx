c3522b22cc315360f206b963f135127ef629d728
mahimasingh04 <mahiisinghh1802@gmail.com>
2025-06-06 11:12:10 +0530

    hydration error

--- apps/user-app/provider.tsx
+++ apps/user-app/provider.tsx
@@ -2,9 +2,11 @@
 import { RecoilRoot } from "recoil";
 import { SessionProvider } from "next-auth/react";
 
+
 export const Providers = ({children}: {children: React.ReactNode}) => {
     return <RecoilRoot>
-        <SessionProvider>
+
+        <SessionProvider> 
             {children}
         </SessionProvider>
     </RecoilRoot>
